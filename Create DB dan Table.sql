6ac93d9cee5a119e9c3a7f82fce913e96e019734
Hizkia sekeon <145413729+hizkiarenvil@users.noreply.github.com>
2024-05-13 14:47:53 +0800

    Add files via upload

---
+++ Create DB dan Table.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE DWH;
+
+
+USE DWH;
+CREATE TABLE DimCustomer (
+    CustomerID INT PRIMARY KEY,
+    CustomerName VARCHAR(50),
+    Address VARCHAR(255),
+    CityName VARCHAR(50),
+    StateName VARCHAR(50),
+    Age VARCHAR(3),
+    Gender VARCHAR(10),
+    Email VARCHAR(50)
+);
+CREATE TABLE DimAccount (
+    AccountID INT PRIMARY KEY,
+    CustomerID INT,
+    AccountType VARCHAR(10),
+    Balance INT,
+    DateOpened DATETIME2(0),
+    status VARCHAR(10),
+    FOREIGN KEY (CustomerID) REFERENCES DimCustomer(CustomerID)
+);
+CREATE TABLE DimBranch (
+    BranchID INT PRIMARY KEY,
+    BranchName VARCHAR(50),
+    BranchLocation VARCHAR(50)
+);
+CREATE TABLE FactTransaction (
+    TransactionID INT PRIMARY KEY,
+    AccountID INT,
+    TransactionDate DATETIME2(0),
+    Amount INT,
+    TransactionType VARCHAR(50),
+    BranchID INT
+    
+    FOREIGN KEY (AccountID) REFERENCES DimAccount(AccountID),
+    FOREIGN KEY (BranchID) REFERENCES DimBranch(BranchID)
+);
---
+++ Stored Procedure.sql
@@ -0,0 +1,47 @@
+------ Pembuatan STORED PROCEDURE
+CREATE PROCEDURE DailyTransaction
+    @start_date DATE,
+    @end_date DATE
+AS
+BEGIN
+    SELECT
+        CONVERT(VARCHAR, t.TransactionDate, 23) AS Date,
+        COUNT(*) AS TotalTransactions,
+        SUM(t.amount) AS TotalAmount
+    FROM
+        FactTransaction t
+    WHERE
+        t.TransactionDate >= @start_date
+        AND t.TransactionDate <= @end_date
+    GROUP BY
+        CONVERT(VARCHAR, t.TransactionDate, 23)
+    ORDER BY
+        Date;
+END;
+
+
+CREATE PROCEDURE BalancePerCustomer
+    @name VARCHAR(255)
+AS
+BEGIN
+    SELECT 
+        c.CustomerName,
+        a.AccountType,
+        a.Balance,
+        a.Balance + SUM(
+            CASE 
+                WHEN t.TransactionType = 'Deposit' THEN t.Amount
+                ELSE -t.Amount
+            END
+        ) AS CurrentBalance
+    FROM DimCustomer c
+    JOIN DimAccount a ON c.CustomerID = a.CustomerID
+    JOIN FactTransaction t ON a.AccountID = t.AccountID
+    WHERE c.CustomerName LIKE '%' + @name + '%' AND a.Status = 'active'
+    GROUP BY c.CustomerName, a.AccountType, a.Balance;
+END;
+
+
+------Menjalankan Stored Procedure
+EXEC DailyTransaction @start_date = '2024-01-18', @end_date = '2024-01-20';
+EXEC BalancePerCustomer @name = 'Shelly';
